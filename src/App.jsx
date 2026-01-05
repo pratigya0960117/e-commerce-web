012c84462b5d3446a0ed011f37611aebef5b1265
pratigya0960117 <spratigya673@gmail.com>
2026-01-05 18:12:08 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import ProductList from "./components/ProductList";
+import Cart from "./components/Cart";
+
+function App() {
+  return (
+    <div className="flex">
+      <ProductList />
+      <Cart />
+    </div>
+  );
+}
+
+export default App;
